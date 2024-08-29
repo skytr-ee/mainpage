8af8d9fabde4545f618c7e2aa4a848679e4db559
AkiChan <arisu@ieee.org>
2024-08-29 13:48:42 +0900

    Initialize the project with essential configuration files, including Tailwind CSS, ESLint, and Next.js setup.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,108 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Rubik } from "next/font/google";
+import {Box, Button, Link, Stack, Text} from "@chakra-ui/react";
+import Head from "next/head";
+import {ReactTyped} from 'react-typed';
 
-const inter = Inter({ subsets: ["latin"] });
+import React from "react";
+
+const rubik = Rubik({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <>
+          <Head>
+              <title>SKYTREE</title>
+              <meta name="author" content="SKYTREE" />
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <meta name="robots" content="index, follow" />
+              <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          </Head>
+          <main
+              className={`flex min-h-screen flex-col items-center justify-between p-24`}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+              <Box textAlign='center'>
+                  <Text
+                      as='h1'
+                      maxW='16ch'
+                      mx='auto'
+                      fontSize={{ base: '2.25rem', sm: '3rem', lg: '4rem' }}
+                      fontFamily = {rubik.className}
+                      letterSpacing='tighter'
+                      fontWeight='extrabold'
+                      mb='16px'
+                      lineHeight='1.2'
+                  >
+                      <Text color='teal.500' _dark={{ color: 'teal.300' }}>
+                          SKYTREE
+                      </Text>
+                  </Text>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+                  <Text
+                      maxW='760px'
+                      mx='auto'
+                      color='gray.500'
+                      _dark={{ color: 'gray.400' }}
+                      fontSize={{ base: 'lg', lg: '3xl' }}
+                      mt='6'
+                  >
+                      SKYTREE is a fast, secure, and reliable decentralized Web3.0 platform
+                  </Text>
+                  <Text
+                        maxW='760px'
+                        mx='auto'
+                        fontWeight='bold'
+                        className={`bg-gradient-to-r from-teal-400 to-teal-500 bg-clip-text text-transparent`}
+                        fontSize={{ base: 'xl', lg: '3xl' }}
+                        mt='6'
+                        >
+                      for&nbsp;
+                      <ReactTyped
+                          strings={[
+                              'Workflow Automation',
+                              'Social Media',
+                              'E-commerce',
+                              'NFTs',
+                              'Blockchain Development',
+                              'Decentralized Finance',
+                          ]}
+                          typeSpeed={40}
+                          backSpeed={50}
+                          loop
+                      />
+                  </Text>
+                  <Stack
+                      mt='10'
+                      gap='4'
+                      justify='center'
+                      direction={{ base: 'column', sm: 'row' }}
+                  >
+                      <Button
+                          h='4rem'
+                          px='40px'
+                          fontSize='1.2rem'
+                          size='lg'
+                          colorScheme='teal'
+                          variant='solid'
+                      >
+                          Get Started
+                      </Button>
+                      <Button
+                          h='4rem'
+                          px='40px'
+                          fontSize='1.2rem'
+                          size='lg'
+                          colorScheme='teal'
+                          variant='outline'
+                          onClick={() => window.open('https://github.com/skytr-ee')}
+                      >
+                          GitHub
+                      </Button>
+                  </Stack>
+              </Box>
+      </main>
+      </>
   );
 }
+
+
