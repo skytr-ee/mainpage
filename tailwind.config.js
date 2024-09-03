8087669d8aa08174be6c4ee9101f9ee194fc3a6e
AkiChan <arisu@ieee.org>
2024-09-03 14:12:06 +0800

    update license

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
